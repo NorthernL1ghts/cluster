b66bda34696ae0ae1043d5a2c141e442146bab2b
Cameron Smith <34502005+NorthernL1ghts@users.noreply.github.com>
2021-11-08 19:56:02 +0000

    Create test.standalone.files.js

---
+++ test/test.standalone.files.js
@@ -0,0 +1,13 @@
+/* Module dependencies */
+
+var cluster = require('../');
+
+cluster = cluster('./support/standalone')
+  .set('workers', 4)
+  .start();
+
+if (cluster.isMaster) {
+  setTimeout(function(){
+    process.kill(process.pid, 'SIGQUIT');
+  }, 500);
+}
